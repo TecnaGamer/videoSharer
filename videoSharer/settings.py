431253a2ba43507940432de09df0a1211344824b
TecnaGamer <34553765+TecnaGamer@users.noreply.github.com>
2021-03-02 14:24:20 -0500

    Procfile

--- videoSharer/settings.py
+++ videoSharer/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
-import django_heroku
+#import django_heroku
 #from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -127,4 +127,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 #Activate Django-Heroku.
-django_heroku.settings(locals())
+#django_heroku.settings(locals())
